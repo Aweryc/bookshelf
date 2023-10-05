5b2611e773dab78b22eff4c0eb42433d42e3d2eb
Aweryc <aweryc@gmail.com>
2023-10-05 20:49:39 +0300

    Registration, books view and book view

--- bookshelf/website/urls.py
+++ bookshelf/website/urls.py
@@ -1,13 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('logout/', views.logout_user, name='logout'),
-    path('register/', views.register_user, name='register'),
-    path('book/<int:pk>', views.book, name='book'),
-    path('delete/<int:pk>', views.delete_record, name='delete_book'),
-    path('add_book/', views.add_record, name='add_book'),
-    path('update/<int:pk>', views.update_record, name='update_book'),
-]
+                  path('', views.home, name='home'),
+                  path('logout/', views.logout_user, name='logout'),
+                  path('register/', views.register_user, name='register'),
+                  path('book/<int:pk>', views.book, name='book'),
+                  path('delete/<int:pk>', views.delete_record, name='delete_book'),
+                  path('add_book/', views.add_record, name='add_book'),
+                  path('update/<int:pk>', views.update_record, name='update_book'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
